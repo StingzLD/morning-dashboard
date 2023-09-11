5eda4438654238f6acd364144232fca05d439691
Corey Hastings <stingzld@gmail.com>
2023-09-11 01:36:54 -0500

    Completed static menu page

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from datetime import datetime
 from menu import get_meal, get_menu
-from weather import get_weather_today
-
-menu = [{
-        'date': '2023-09-10',
-        'dayOfWeek': 'Monday',
-        'main': 'Peanut Butter & Jelly',
-        'side1': 'Berry Medley',
-        'side2': 'Yogurt',
-        'snack': 'Goldfish',
-        'id': 2
-    }]
-# menu = get_menu("schoolLunch")
-# print(data)
 
 raw_date = datetime.today()
 day_of_week = raw_date.strftime("%A")
 date = raw_date.strftime("%Y-%m-%d")
-print(day_of_week, date)
 
-lunch_today = get_meal(menu, date)
-print(lunch_today)
+menu = [{
+    'date': '2023-09-10',
+    'dayOfWeek': 'Monday',
+    'main': 'Peanut Butter & Jelly',
+    'side1': 'Berry Medley',
+    'side2': 'Yogurt',
+    'snack': 'Goldfish',
+    'id': 2
+}]
+# menu = get_menu("schoolLunch")
+# print(menu)
+main, side1, side2, snack = get_meal(menu, date)
+print(main, side1, side2, snack)
