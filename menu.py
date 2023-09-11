b0ccaeeb60e9c04ecfb6bf4bea712649a2ba946e
Corey Hastings <stingzld@gmail.com>
2023-09-10 23:28:21 -0500

    Completed menu acquisition

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from datetime import datetime
+
 
 SHEETY_ENDPOINT = "https://api.sheety.co"
 SHEETY_USERNAME = os.environ['SHEETY_USERNAME']
@@ -7,13 +9,51 @@ SHEETY_BEARER_TOKEN = os.environ['SHEETY_BEARER_TOKEN']
 
 project_url = f"{SHEETY_ENDPOINT}/{SHEETY_USERNAME}/menus"
 headers = {
-            "Authentication": f"Bearer {SHEETY_BEARER_TOKEN}",
-            "Content-Type": "application/json"
-        }
+    "Authorization": f"Bearer {SHEETY_BEARER_TOKEN}",
+    "Content-Type": "application/json"
+}
 
-response = requests.get(
-    url=f"{project_url}/lunch",
-    headers=headers
-)
-response.raise_for_status()
-print(response.text)
+
+def get_menu(menu):
+    response = requests.get(
+        url=f"{project_url}/{menu}",
+        headers=headers
+    )
+
+    if response.status_code == 402:
+        print("Monthly quota exceeded. Payment Required.")
+        return 402
+    else:
+        return response.json()[f'{menu}']
+
+
+def get_meal(menu, date):
+    for meal in menu:
+        if date == meal['date']:
+            main = meal['main']
+            side1 = meal['side1']
+            side2 = meal['side2']
+            snack = meal['snack']
+            return main, side1, side2, snack
+
+
+if __name__ == '__main__':
+    menu = [{
+        'date': '2023-09-10',
+        'dayOfWeek': 'Monday',
+        'main': 'Peanut Butter & Jelly',
+        'side1': 'Berry Medley',
+        'side2': 'Yogurt',
+        'snack': 'Goldfish',
+        'id': 2
+    }]
+    # menu = get_menu("schoolLunch")
+    # print(data)
+
+    raw_date = datetime.today()
+    day_of_week = raw_date.strftime("%A")
+    date = raw_date.strftime("%Y-%m-%d")
+    print(day_of_week, date)
+
+    lunch_today = get_meal(menu, date)
+    print(lunch_today)
