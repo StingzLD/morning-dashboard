c06f316bd5664cb04f475b4cafd5f2cc7b120ff7
Corey Hastings <stingzld@gmail.com>
2025-03-18 14:12:57 -0500

    Added menu logic to app route | Updated template to use variables

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from date import get_date
+from menu import get_meal, get_menu
 
 app = Flask(__name__)
 
@@ -15,7 +17,15 @@ def weather():
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    date, day_of_week = get_date()
+    menu_data = get_menu("schoolLunch")
+    main, side1, side2, snack = get_meal(menu_data, date)
+    return render_template('menu.html',
+                           dayOfWeek=day_of_week,
+                           main=main,
+                           side1=side1,
+                           side2=side2,
+                           snack=snack)
 
 
 if __name__ == '__main__':
